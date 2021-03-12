ecfa8f9823eacb98123165fecf3aea3a8486780c
esleyder1 <megaesleyder@gmail.com>
2021-03-12 07:07:53 -0500

    firebase was connected

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: '35e8ab2ded52eccbc48e6dd76955f31f4d271e06',
+    authDomain: 'fc-socio-familiar.firebaseapp.com',
+    databaseURL: 'https://fc-socio-familiar.firebaseio.com',
+    projectId: 'fc-socio-familiar',
+    storageBucket: 'fc-socio-familiar.appspot.com',
+    messagingSenderId: 'xxx',
+    appId: 'fc-socio-familiar'
+  }
+};
