5eaddb4f59f86feb2869cc52d7d431e1be17a3c6
esleyder <megaesleyder@gmail.com>
2021-04-08 07:20:53 -0500

    Web with capacitor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
